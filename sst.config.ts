53fcead0372ffb075fde4ad12e903fe328e0a2f1
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-07-19 15:16:35 +0100

    Use SST for svelte only

--- sst.config.ts
+++ sst.config.ts
@@ -1,17 +1,15 @@
 import { SSTConfig } from "sst";
 import { Web } from "./stacks/Web";
-import { Database } from "./stacks/Database";
 
 export default {
     config(_input) {
         return {
             name: "recommend-books-2",
-            region: "us-east-1",
+            region: "eu-north-1",
         };
     },
     stacks(app) {
         app
-            .stack(Database)
             .stack(Web);
     }
 } satisfies SSTConfig;
