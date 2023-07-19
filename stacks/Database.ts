65b3c6d75bf4842b592d2f4b5d92ec7dc28c610e
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-07-19 20:07:53 +0100

    Switch back to basic sveltekit project

--- stacks/Database.ts
+++
@@ -1,9 +0,0 @@
-import { RDS, StackContext } from "sst/constructs";
-
-export function Database({ stack }: StackContext) {
-    new RDS(stack, "db", {
-        engine: "postgresql11.13",
-        defaultDatabaseName: "main",
-        migrations: "packages/core/migrations",
-    });
-}
